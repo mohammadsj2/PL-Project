03a458d9b4c819a7531f4d17a8d3607a86b2f8e9
mohammadsj2 <mohammad.sj2@gmail.com>
2021-07-15 03:33:44 +0430

    Clean file_reader.rkt

--- file_reader.rkt
+++ file_reader.rkt
@@ -1,20 +1,8 @@
 #lang racket
 (provide read-instructions-from-file)
-(define-namespace-anchor anc)
-(define ns (namespace-anchor->namespace anc))
-
-(define (append-strings lines)
-  (cond
-    ((null? lines) "")
-    (else
-     (string-append (car lines) (append-strings (cdr lines)))
-     )
-    )
-  )
 
 (define (read-instructions-from-file input_addr)
-    (append-strings (file->lines input_addr))
+    (apply string-append (file->lines input_addr))
   )
-
 
 ;(read-instructions-from-file "program.txt")
